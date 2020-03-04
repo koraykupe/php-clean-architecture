9b746a4afd2260a7460b3606a1eac2a777a12f15
Koray Kupe <koray.kupe@leisure-group.eu>
2020-03-04 14:29:35 +0100

    Add identity (uuid) generation for article id value class

--- article/domain/Value/ArticleId.php
+++ article/domain/Value/ArticleId.php
@@ -1,7 +1,30 @@
 <?php
 
+use Ramsey\Uuid\Uuid;
 
 class ArticleId
 {
+    private $id;
 
+    /**
+     * Immutable, so generation from construct is private
+     * ArticleId constructor.
+     * @param null $id
+     * @throws Exception
+     */
+    private function __construct($id = null)
+    {
+        $this->id = $id ?: Uuid::uuid4()->toString();
+    }
+
+    /**
+     * @param null $id
+     * @returnArticleId
+     * @return ArticleId
+     * @throws Exception
+     */
+    public static function create($id = null ): \ArticleId
+    {
+        return new static($id);
+    }
 }
